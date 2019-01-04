f273183698de298a108c5f850dc93e5582ebc290
redtide <redtid3@gmail.com>
2019-01-04 12:57:04 +0100

    Restore original localization.

--- archiso/airootfs/root/customize_airootfs.sh
+++ archiso/airootfs/root/customize_airootfs.sh
@@ -2,10 +2,10 @@
 
 set -e -u
 
-sed -i 's/#\(it_IT\.UTF-8\)/\1/' /etc/locale.gen
+sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
 
-ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime
+ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 
 usermod -s /usr/bin/zsh root
 cp -aT /etc/skel/ /root/
