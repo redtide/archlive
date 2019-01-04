eae3036dc356327955bb0df06e9549a05c3c5df0
redtide <redtid3@gmail.com>
2019-01-04 11:59:57 +0100

    Changed configuration.

--- archiso/airootfs/root/customize_airootfs.sh
+++ archiso/airootfs/root/customize_airootfs.sh
@@ -2,18 +2,14 @@
 
 set -e -u
 
-sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
+sed -i 's/#\(it_IT\.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
 
-ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+ln -sf /usr/share/zoneinfo/Europe/Rome /etc/localtime
 
 usermod -s /usr/bin/zsh root
 cp -aT /etc/skel/ /root/
-useradd -m -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/zsh liveuser
-#chmod 700 /root
-chown -R liveuser:users /home/liveuser
-
-
+chmod 700 /root
 
 sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
@@ -25,6 +21,16 @@ sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 
 systemctl enable pacman-init.service choose-mirror.service
 systemctl set-default multi-user.target
+
+useradd -m -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/bash arch
+echo "root:master" | chpasswd
+echo "arch:live" | chpasswd
+chown -R arch:users /home/arch
+echo 'arch	ALL=(ALL:ALL) ALL' >> /etc/sudoers
+echo 'arch	ALL=NOPASSWD: /usr/bin/arch-install' >> /etc/sudoers
+
+chmod +x /usr/bin/arch-install
+
 systemctl enable NetworkManager
+
 rm /etc/udev/rules.d/81-dhcpcd.rules
-pacman -Syy
